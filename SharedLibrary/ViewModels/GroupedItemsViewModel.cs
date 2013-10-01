732015b044fc264ad75c869649a47a8838bf6fe6
Johan Lindfors <johan.lindfors@live.com>
2013-10-01 22:31:18 +0200

    GroupedItemsViewModel not used yet

---
+++ SharedLibrary/ViewModels/GroupedItemsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using SharedLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.ViewModels
+{
+    public class GroupedItemsViewModel 
+    {
+        private ObservableCollection<SampleDataGroup> groups = new ObservableCollection<SampleDataGroup>();
+        public ObservableCollection<SampleDataGroup> Groups
+        {
+            get { return groups; }
+        }
+    }
+}
